7e329f844a573bd2d53cec0d84b2ed08c05bacc6
kamal <kamal.ayman522@gmail.com>
2024-01-05 20:59:22 +0200

    update

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%e-os+d3djpij3#)97+bph6x)%nr4--q$!)hr^c+m9bj6i=(-e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.2', 'mychurch.azurewebsites.net']
 
